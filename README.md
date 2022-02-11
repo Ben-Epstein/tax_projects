57574099b61e8c23adc084eede735cf349d07642
Ben Epstein <ben.epstein@wustl.edu>
2022-02-11 16:21:08 -0500

    Add files via upload
    Signed-off-by: Ben Epstein <ben@rungalileo.io>
    Signed-off-by: Ben Epstein <ben.epstein97@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# tax_document_converter
-A simple script to convert tax documents
+# Tax File Name Converter
+
+## Description
+This python script will convert raw (Schwab formatted) tax return
+documents into properly named files, and copy them from the source
+directory (folder) location to the destination, as  provided by the prompts. 
+
+## Installing
+To use this script, you will need the following:
+1. Python >= 3.7. Download [here](https://www.python.org/downloads/)
+2. `pip` (pythons package manager). Run `py -m ensurepip --upgrade`
+3. Supporting python packages:
+   1. `pandas`
+   2. `tqdm`
+
+Install these by running `pip install -r requirements.txt`
+
+## Running
+To run this script, double click on the python file, or, from the terminal, run:
+`py tax_document_converter.py`
+
+The program will prompt you for the following information:
+1. The input path to the tax PDF files
+2. The top level folder containing all CPA and Client folders
+3. The tax filing year
+4. The path and name of the spreadsheet containing the CPA client information
+5. The path and name of the spreadsheet containing the mapping of client ID to household ID
+
+**Please make sure to provide all responses as full paths. They should start with `C:\\` or similar (depending on the drive)**
---
+++ tax_document_converter.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+import os
+import pandas as pd
+from tqdm import tqdm
+import shutil
+from glob import glob
+from pathlib import PureWindowsPath
+
+
+def convert_and_copy():
+    file_loc = PureWindowsPath(
+        input("Please provide the full path to the input files.")
+    )
+    output_loc = PureWindowsPath(
+        input(
+            "Please provide the full path the final output folder. "
+            "All CPA folders should be here."
+        )
+    )
+    cur_year = input(
+        "What year are we filing in? This number should match the year of the folder "
+        "we are adding the files to "
+        "(For example, in year 2022 filing taxes for 2021, enter 2022)"
+    )
+
+    must_be_pdf = True
+    all_files = [file for file in os.listdir(file_loc) if os.path.isfile(file)]
+
+    if must_be_pdf:
+        all_files = [file for file in all_files if file.endswith("pdf")]
+
+    # Mapping data for account numbers to CPA data
+    cpa_cols = ["Tax Info Recipient 1: Full Name", "Tax Info Recipient 2: Full Name"]
+    client_col = "Account Name"
+    acct_num_col = "Financial Account Name"
+    custodian_name = "Custodian: Custodian Name"
+    household_id = "Source System ID"
+
+    cols = cpa_cols + [client_col] + [acct_num_col] + [custodian_name] + [household_id]
+    cpa_file = input(
+        "Please provide the full path and file name to the CPA Tax Spreadsheet"
+    )
+    df = pd.read_excel(cpa_file, skiprows=11, usecols=cols)
+    df = df[df[custodian_name] == "Charles Schwab & Co."]
+
+    # Mapping data for financial account number to household number 
+    account_to_household_key = "Financial Account: Financial Account Name"
+    account_to_household_value = "Household: Source System ID"
+    household_prefix = "Household-"
+    household_mapping_file = input(
+        "Please provide the full path and file name to the "
+        "Financial Accounts Household ID file"
+    )
+    mapping_cols = [account_to_household_key, account_to_household_value]
+    household_mapping = pd.read_excel(
+        household_mapping_file, skiprows=9, usecols=mapping_cols)
+    household_mapping = household_mapping.set_index(
+        account_to_household_key
+    ).to_dict()[account_to_household_value]
+
+    # Clean up unicode char (\xa0)
+    df = df.fillna("None")
+    for c in cpa_cols:
+        df[c] = df[c].apply(lambda row: row.strip())
+
+    print(f"Processing {len(all_files)} files")
+
+    all_clients = glob(f"{output_loc}/**/*{cur_year}/", recursive=True)
+
+    # Structure is 1234-5678_rest_of_name.pdf
+    # First 8 digits are account #, first 4 must be redacted
+    cleaned_files = []
+    for file in tqdm(all_files):
+        acct_number, *rest_of_file = file.split("_")
+        acct_number = acct_number.replace("-", "")
+        rest_of_file = "_".join(rest_of_file)
+        redacted_acct_number = "X" * 4 + acct_number[4:]
+
+        df_for_acct = df[df[acct_num_col] == acct_number]
+        client = df_for_acct[client_col].values[0]
+
+        file_name = f"{client}_{redacted_acct_number}_{rest_of_file}"
+        cleaned_files.append(file_name)
+
+        household_id = household_mapping[acct_number].lstrip(household_prefix)
+        for client_dir in all_clients:
+            if household_id in str(client_dir):
+                # This is the right client. Write the file
+                input_file = PureWindowsPath(file)
+                output_file = PureWindowsPath(client_dir) / file_name
+                shutil.copyfile(input_file, output_file)
+
+    
+if __name__ == "__main__":
+    convert_and_copy()
